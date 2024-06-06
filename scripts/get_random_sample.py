d8fd1061089cc40dd029709c86dd45742c7b526b
Ben Steel <bendavidsteel@gmail.com>
2024-06-06 11:18:32 +0200

    Testing multi curl func

--- scripts/get_random_sample.py
+++ scripts/get_random_sample.py
@@ -428,6 +428,10 @@ class ProcessVideo:
             )
         return video_info
 
+def process_video(text, headers=None):
+    video_processor = ProcessVideo(headers=headers)
+    video_processor.process_chunk(text)
+    return video_processor.process_response()
 
 class PyCurlResponse:
     status_code: int
