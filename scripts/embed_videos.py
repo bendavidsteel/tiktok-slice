8dbaa2d920a4abb27034d24e3b86a8f28ff05e14
Ben Steel <bendavidsteel@gmail.com>
2024-06-13 17:48:23 +0200

    Working on video embedding

--- scripts/embed_videos.py
+++ scripts/embed_videos.py
@@ -38,19 +38,26 @@ def sample_frame_indices(clip_len, frame_sample_rate, seg_len):
     Returns:
         indices (`List[int]`): List of sampled frame indices
     '''
-    converted_len = int(clip_len * frame_sample_rate)
-    end_idx = np.random.randint(converted_len, seg_len)
-    start_idx = end_idx - converted_len
+    # converted_len = int(clip_len * frame_sample_rate)
+    # if seg_len <= converted_len:
+    #     raise ValueError(f"Video is too short to sample {clip_len} frames with sample rate {frame_sample_rate}")
+    # end_idx = np.random.randint(converted_len, seg_len)
+    # start_idx = end_idx - converted_len
+    start_idx = 0
+    end_idx = seg_len
     indices = np.linspace(start_idx, end_idx, num=clip_len)
     indices = np.clip(indices, start_idx, end_idx - 1).astype(np.int64)
     return indices
 
 def load_video(video_file_path):
-    with av.open(video_file_path) as container:
-        # sample 16 frames
-        indices = sample_frame_indices(clip_len=8, frame_sample_rate=1, seg_len=container.streams.video[0].frames)
-        video = read_video_pyav(container, indices)
-    return video
+    try:
+        with av.open(video_file_path) as container:
+            # sample 16 frames
+            indices = sample_frame_indices(clip_len=8, frame_sample_rate=1, seg_len=container.streams.video[0].frames)
+            video = read_video_pyav(container, indices)
+        return video
+    except Exception as e:
+        raise
 
 class MultiModalBackend:
     def __init__(self):
@@ -93,12 +100,13 @@ class MultiModalBackend:
 
 def embed_directory(embedding_model, video_df, dir_path):
     host_file_paths = [os.path.join(dir_path, server_filename) for server_filename in os.listdir(dir_path) if server_filename.endswith('.mp4')]
+    host_file_paths = sorted(host_file_paths)
     host_file_paths = host_file_paths[200:]
     byte_video_ids = [os.path.splitext(os.path.basename(host_file_path))[0] for host_file_path in host_file_paths]
 
     # get video data for each video
     video_df['return'] = video_df['result'].map(lambda r: r['return'])
-    video_df['id'] = video_df['return'].map(lambda r: r['id'])
+    video_df['id'] = video_df['return'].map(lambda r: r['id'] if r and 'id' in r else None)
     video_df = video_df[['return', 'id']].rename(columns={'return': 'video'})
     video_df = video_df[video_df['id'].map(lambda id: id is not None)]
     meta_video_ids = video_df['id'].tolist()
@@ -112,20 +120,34 @@ def embed_directory(embedding_model, video_df, dir_path):
     video_df = video_df.loc[bytes_video_id_order]
     video_df = video_df.reset_index()
 
+    if len(host_file_paths) == 0:
+        return
+
     embedding_path = os.path.join(dir_path, 'video_embeddings.npy')
     video_path = os.path.join(dir_path, 'videos.parquet.gzip')
 
+    add_to_existing = False
     if os.path.exists(embedding_path) and os.path.exists(video_path):
-        embeddings = np.load(embedding_path)
-        saved_video_df = pd.read_parquet(video_path)
-
-        if embeddings.shape[0] == video_df.shape[0] and all(saved_video_df['id'] == video_df['id']):
-            return
+        try:
+            saved_embeddings = np.load(embedding_path)
+            saved_video_df = pd.read_parquet(video_path)
+            
+            if saved_embeddings.shape[0] == saved_video_df.shape[0]:
+                saved_video_ids = set(saved_video_df['id'].tolist())
+                video_ids = set(video_df['id'].tolist())
+                if saved_video_ids == video_ids:
+                    return
+                elif saved_video_ids.issubset(video_ids):
+                    add_to_existing = True
+                    video_df = video_df[~video_df['id'].isin(saved_video_ids)]
+                    host_file_paths = [host_file_path for host_file_path in host_file_paths if os.path.splitext(os.path.basename(host_file_path))[0] in video_df['id'].tolist()]
+        except Exception as e:
+            print(f"Failed to load embeddings: {e}")
 
     embeddings = None
     i = 0
     max_batch_file_size = 5e7
-    max_batch_size = 8
+    max_batch_size = 64
     pbar = tqdm.tqdm(total=len(host_file_paths))
     while i < len(host_file_paths):
         batch_file_size = 0
@@ -143,12 +165,26 @@ def embed_directory(embedding_model, video_df, dir_path):
             batch_embeddings = embedding_model.embed_videos(batch_file_paths)
         except Exception as e:
             print(f"Failed to embed videos: {e}")
+            video_df = video_df[~video_df['id'].isin([os.path.splitext(os.path.basename(file_path))[0] for file_path in batch_file_paths])]
             continue
         pbar.update(batch_size)
         if embeddings is None:
             embeddings = batch_embeddings
         else:
             embeddings = np.concatenate([embeddings, batch_embeddings], axis=0)
+
+    if embeddings is None:
+        return
+
+    assert embeddings.shape[0] == video_df.shape[0]
+    assert embeddings.shape[0] > 0
+
+    if add_to_existing:
+        embeddings = np.concatenate([saved_embeddings, embeddings], axis=0)
+        video_df = pd.concat([saved_video_df, video_df], axis=0)
+
+    assert embeddings.shape[0] == video_df.shape[0]
+    assert embeddings.shape[0] > 0
 
     with open(embedding_path, 'wb') as f:
         np.save(f, embeddings)
@@ -162,8 +198,8 @@ def embed_directory(embedding_model, video_df, dir_path):
 async def main():
     this_dir_path = os.path.dirname(os.path.realpath(__file__))
     root_dir_path = os.path.dirname(this_dir_path)
-    host_bytes_dir_path = os.path.join(root_dir_path, 'data', 'bytes')
-    host_results_dir_path = os.path.join(root_dir_path, 'data', 'results')
+    host_results_dir_path = os.path.join('/', 'mnt', 'bigone', 'bsteel', 'tiktok', 'data', 'results')
+    host_bytes_dir_path = os.path.join('/', 'mnt', 'bigone', 'bsteel', 'tiktok', 'data', 'bytes')
 
     embedding_model = MultiModalBackend()
 
--- scripts/topic_model_videos.py
+++ scripts/topic_model_videos.py
@@ -5,17 +5,21 @@ import os
 import av
 from bertopic import BERTopic
 from bertopic.representation import VisualRepresentation
+import dotenv
 import numpy as np
 import pandas as pd
 
 def get_video_path(video):
-    this_dir_path = os.path.dirname(os.path.realpath(__file__))
-    bytes_dir_path = os.path.join(this_dir_path, '..', 'data', 'bytes')
+    data_dir_path = os.environ['DATA_DIR_PATH']
+    bytes_dir_path = os.path.join(data_dir_path, 'bytes')
     video_id = video['id']
     timestamp = int(format(int(video_id), '064b')[:32], 2)
     return os.path.join(bytes_dir_path, str(timestamp), f"{video_id}.mp4")
 
 def save_first_frame(video_path):
+    frame_path = video_path.replace('.mp4', '.jpg')
+    if os.path.exists(frame_path):
+        return frame_path
     container = av.open(video_path)
     for frame in container.decode(video=0):
         frame.to_image().save(video_path.replace('.mp4', '.jpg'))
@@ -23,8 +27,9 @@ def save_first_frame(video_path):
     return video_path.replace('.mp4', '.jpg')
 
 def main():
+    dotenv.load_dotenv()
     this_dir_path = os.path.dirname(os.path.realpath(__file__))
-    bytes_dir_path = os.path.join(this_dir_path, '..', 'data', 'bytes')
+    bytes_dir_path = os.path.join(os.environ['DATA_DIR_PATH'], 'bytes')
     embeddings = None
     video_df = None
     for dir_name in os.listdir(bytes_dir_path):
@@ -70,9 +75,9 @@ def main():
     # Train our model with images only
     topic_model = BERTopic(representation_model=representation_model, verbose=True)
     topics, probs = topic_model.fit_transform(
-        documents=video_df['video'].map(lambda v: v['desc']), 
+        documents=video_df['video'].map(lambda v: v['desc']).tolist(), 
         embeddings=embeddings,
-        images=video_df['image']
+        images=video_df['image'].tolist()
     )
 
     df = topic_model.get_topic_info()
